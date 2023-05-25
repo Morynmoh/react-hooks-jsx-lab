386786770255193db432ccfb5f097a0418e2d74f
Maureen <morynmoh@gmail.com>
2023-05-25 22:27:22 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div>
+    <h2>About Me</h2>
+    <p>I love React Web Development. It Makes life much Easier</p>
+    <img src={image} alt="I made this" />
+  </div>);
 }
 
 export default About;
